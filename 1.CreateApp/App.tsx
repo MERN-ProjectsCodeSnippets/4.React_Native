dec473266ae2c86f0662c6021ffb7df8f3e3be11
@teslamd02 <teslamd02@gmail.com>
2023-05-04 12:25:41 +0530

    addedd code of props

--- 1.CreateApp/App.tsx
+++ 1.CreateApp/App.tsx
@@ -1,18 +1,20 @@
-import { View, Text, Button } from 'react-native'
-import React, { useState } from 'react'
+import { View, Text,StyleSheet } from 'react-native'
+import React from 'react'
 
 const App = () => {
-  const [data,setData]= useState('This is my Initial data');
-
-  const changeText = ()=>{
-    setData("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod tristique magna, in rhoncus nisi. Integer non lacus non lorem ullamcorper egestas. Quisque pharetra interdum sapien id pellentesque. Duis sit amet justo mauris. Curabitur auctor, enim sed convallis consequat, elit est dapibus arcu, vel scelerisque nunc justo in lectus. Etiam vel nulla sit amet odio euismod convallis. Suspendisse suscipit imperdiet tellus eu hendrerit. Aliquam auctor ante at risus sagittis, eget maximus lacus")
-  }
   return (
-    <View>
-      <Text>{data}</Text>
-      <Button title='click to update Text' onPress={changeText}>Click to Update the state</Button>
+    <View style={styles.container}>
+      <Text style={{color:'blue','fontSize':30,'fontWeight':300,'margin-top':100}}>Welocme To First Application</Text>
     </View>
   )
 }
 
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor:'#f4dbdb',
+  }
+})
 export default App
+
